8342f8a38b68f524f561803226e861c31895f9b8
Ashish Yadav <aybhu10@gmail.com>
2024-09-17 23:50:54 +0530

    env file setup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY="AIzaSyD4lwYszU-HOiyDpVL7_RyZtsoYiewbxwQ"
+const GOOGLE_API_KEY=process.env.REACT_APP_GOOGLE_API_KEY
 
 export const YOUTUBE_VIDEO_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY
 
